20ab5931aa3f91e2a20750e4d129fa5b645ef85a
castle-bird <seongjo0501@gmail.com>
2026-01-22 22:04:08 +0900

    feat: File 다운로드 작업

--- src/main/java/com/wc/hr_bank/mapper/FileMapper.java
+++ src/main/java/com/wc/hr_bank/mapper/FileMapper.java
@@ -1,5 +1,12 @@
 package com.wc.hr_bank.mapper;
 
+import com.wc.hr_bank.dto.response.file.FileDto;
+import com.wc.hr_bank.entity.File;
+import org.mapstruct.Mapper;
+import org.springframework.core.io.Resource;
+
+@Mapper(componentModel = "spring")
 public interface FileMapper
 {
+    FileDto toFileDto(File file, Resource resource);
 }
