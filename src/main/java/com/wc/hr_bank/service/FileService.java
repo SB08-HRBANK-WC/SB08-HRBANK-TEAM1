644aeef1ffdbf7a097a1f0e73a6c6b5f25860c4e
castle-bird <167004937+castle-bird@users.noreply.github.com>
2026-01-23 10:01:59 +0900

    File 다운로드 로직 관련 (#2)
    * feat: File 다운로드 작업
    
    * feat: File 포메팅 구글 스타일로 변경
    
    * feat: File 컨트롤러 포멧 수정

--- src/main/java/com/wc/hr_bank/service/FileService.java
+++ src/main/java/com/wc/hr_bank/service/FileService.java
@@ -1,5 +1,24 @@
 package com.wc.hr_bank.service;
 
+import com.wc.hr_bank.dto.response.file.FileDto;
+import com.wc.hr_bank.entity.File;
+import org.springframework.core.io.Resource;
+
 public interface FileService
 {
+  /**
+   * 실제 저장된 파일을 가져오기 위한 메서드,
+   *
+   * @param file FileEntity
+   * @return Binary File
+   */
+  Resource getFile(File file);
+
+  /**
+   * 파일을 다운로드 하기 위한 메서드,
+   *
+   * @param fileId 다운로드 받으려는 File의 ID
+   * @return 다운로드 됨
+   */
+  FileDto fileDownload(Long fileId);
 }
